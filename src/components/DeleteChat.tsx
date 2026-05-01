bef344d7713452782085979bc3837c192c927b0f
Yokesh <yokeshbaskaran@gmail.com>
2026-05-01 20:22:02 +0530

    delete modal modified

--- src/components/DeleteChat.tsx
+++ src/components/DeleteChat.tsx
@@ -9,29 +9,31 @@ type DeleteChatProps = {
 const DeletePopup = ({ onCancel, onConfirm, title }: DeleteChatProps) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black/40 z-50">
-      <div className="bg-white rounded-xl p-10 w-88 shadow-lg">
+      <div className="bg-bg-hover rounded-xl p-10 w-88 shadow-lg">
         <div className="flex flex-col items-center gap-3">
           <IoWarningOutline size={28} color="red" />
-          <h2 className="text-xl font-semibold text-center">Delete Chat?</h2>
+          <h2 className="text-xl text-text font-semibold text-center">
+            Delete Chat?
+          </h2>
 
-          <p className="text-sm text-gray-500 text-center mb-6">
+          <p className="text-sm text-text text-center mb-6">
             Are you sure you want to delete{" "}
-            <span className="text-green-600 font-medium">"{title}"</span>? This
+            <span className="text-primary font-medium">"{title}"</span>? This
             action cannot be undone.
           </p>
         </div>
 
-        <div className="flex justify-between gap-3">
+        <div className="flex text-text justify-between gap-3">
           <button
             onClick={onCancel}
-            className="w-full py-2 rounded bg-gray-200"
+            className="w-full py-2 rounded bg-bg cursor-pointer"
           >
             Cancel
           </button>
 
           <button
             onClick={onConfirm}
-            className="w-full py-2 rounded bg-red-500 text-white"
+            className="w-full py-2 rounded bg-red-500 text-text cursor-pointer"
           >
             Delete
           </button>
