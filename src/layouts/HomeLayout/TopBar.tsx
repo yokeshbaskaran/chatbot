a8e5c2939795fe7d4bcde28eb23424d0e2b6e224
Yokesh <yokeshbaskaran@gmail.com>
2026-05-01 21:13:55 +0530

    dark set in localstorage and framer motion is added

--- src/layouts/HomeLayout/TopBar.tsx
+++ src/layouts/HomeLayout/TopBar.tsx
@@ -1,9 +1,7 @@
 import { RiMenuUnfold3Line, RiMenuUnfold4Line } from "react-icons/ri";
-
 import { useAppContext } from "../../context/AppContext";
-
 import DarkMode from "../../utils/DarkMode";
-import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   const { openSideBar, setOpenSideBar, pathToHome } = useAppContext();
@@ -17,26 +15,44 @@ const Navbar = () => {
     <>
       <div className="h-14 px-3 py-5 border-b border-border flex items-center bg-bg">
         {/* Sidebar Menu */}
-        <div
+        <motion.div
+          whileHover={{
+            scale: 1.08,
+          }}
+          whileTap={{
+            scale: 0.92,
+          }}
+          animate={{
+            rotate: openSideBar ? 180 : 0,
+          }}
+          transition={{
+            duration: 0.3,
+          }}
           onClick={handleSidebarToggle}
-          className="px-2 py-2 cursor-pointer hover:bg-bg-hover text-text transition-colors"
+          className="px-2  py-2 cursor-pointer hover:bg-bg-hover text-text transition-colors"
         >
           {openSideBar ? (
             <RiMenuUnfold4Line size={20} />
           ) : (
             <RiMenuUnfold3Line size={20} />
           )}
-        </div>
+        </motion.div>
 
         {/* Logo in topbar  */}
-        <div
+        <motion.div
+          whileHover={{
+            x: 3,
+          }}
+          whileTap={{
+            scale: 0.98,
+          }}
           onClick={pathToHome}
           className="flex  gap-2 ml-2 px-2 py-1 cursor-pointer rounded hover:bg-bg-hover transition-colors"
         >
           <h2 className="text-xl text-text font-semibold flex items-center gap-0">
             Chat <span className="text-blue-400 font-bold">AI</span>
           </h2>
-        </div>
+        </motion.div>
 
         {/* Dark mode toggle  */}
         <div className="ml-auto">
